1c5f494a191d4171e4240d1a9003ac39a6e9a613
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-02-05 09:37:42 -0700

    added projects back

---
+++ projects/mario-pest-control/mario-pest-control.js
@@ -0,0 +1,35 @@
+function calculateGoons(e) {
+    e.preventDefault()
+ var goomba = document.getElementById('goombaData').value;
+ var bobomb = document.getElementById('bob-ombData').value;
+ var cheepcheep = document.getElementById('cheep-cheepData').value;
+ var result = (+goomba*5) + (+bobomb*7) + (+cheepcheep*11)
+ document.getElementById("result").innerHTML = ("Reward: " + result  + " Coins!" );
+ }
+
+ document.getElementById("submitButton").addEventListener('click', calculateGoons)
+
+function clearGoomba(){
+    document.getElementById("goombaData").value = ""
+}
+
+document.getElementById("goombaData").addEventListener("focus", clearGoomba)
+
+function clearbobomb(){
+    document.getElementById("bob-ombData").value = ""
+}
+
+document.getElementById("bob-ombData").addEventListener("focus", clearbobomb)
+
+function clearCheepcheep(){
+    document.getElementById("cheep-cheepData").value = ""
+}
+
+document.getElementById("cheep-cheepData").addEventListener("focus", clearCheepcheep)
+
+// var bleep = new Audio
+// bleep.src = "bleep.mp3"
+// function cash (){
+//     bleep.play()
+// } 
+// document.getElementById("submitButton").addEventListener("click", bleep)
